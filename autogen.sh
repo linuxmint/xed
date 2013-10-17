fa9c24a8d33a6325947faf88d2bd4560ace7ffe9
raveit65 <chat-to-me@raveit.de>
2013-10-18 01:01:11 +0200

    rename to pluma

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="mate-text-editor"
+PKG_NAME="pluma"
 
 (test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
