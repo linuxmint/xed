d30af5ce18ee63efa8f66c5c7089bb4536805ec6
infirit <infirit@gmail.com>
2014-02-22 17:36:10 +0100

    Change file enciding to utf-8 (close #65)

--- pluma/plumatextregion.c
+++ pluma/plumatextregion.c
@@ -4,7 +4,7 @@
  *
  * This file is part of the GtkSourceView widget
  *
- * Copyright (C) 2002 Gustavo Gir·ldez <gustavo.giraldez@gmx.net>
+ * Copyright (C) 2002 Gustavo Gir√°ldez <gustavo.giraldez@gmx.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
