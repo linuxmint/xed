65f91807ca3f5e1d78cc69f004bfd036f7382b8f
infirit <infirit@gmail.com>
2014-12-16 13:05:53 +0100

    Revert "Revert "Drop support for glib < 2.36""
    This reverts commit 13392e50c61ce3f3d7d04b59677380f935cf3d3c.

--- tests/document-loader.c
+++ tests/document-loader.c
@@ -238,9 +238,6 @@ test_begin_new_line_detection ()
 int main (int   argc,
           char *argv[])
 {
-#if !GLIB_CHECK_VERSION (2, 36, 0)
-	g_type_init ();
-#endif
 	g_test_init (&argc, &argv, NULL);
 
 	pluma_prefs_manager_app_init ();
