2fb38137933f0b21367a7d5a81b83d369f502197
JosephMcc <mccullarjoseph@gmail.com>
2017-01-19 01:48:54 -0800

    xed-document-output-stream: Clean up code style

--- xed/xed-document-output-stream.h
+++ xed/xed-document-output-stream.h
@@ -16,7 +16,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with xed; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, 
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
  * Boston, MA  02110-1301  USA
  */
 
@@ -29,33 +29,33 @@
 
 G_BEGIN_DECLS
 
-#define XED_TYPE_DOCUMENT_OUTPUT_STREAM		(xed_document_output_stream_get_type ())
-#define XED_DOCUMENT_OUTPUT_STREAM(obj)		(G_TYPE_CHECK_INSTANCE_CAST ((obj), XED_TYPE_DOCUMENT_OUTPUT_STREAM, XedDocumentOutputStream))
-#define XED_DOCUMENT_OUTPUT_STREAM_CONST(obj)		(G_TYPE_CHECK_INSTANCE_CAST ((obj), XED_TYPE_DOCUMENT_OUTPUT_STREAM, XedDocumentOutputStream const))
-#define XED_DOCUMENT_OUTPUT_STREAM_CLASS(klass)	(G_TYPE_CHECK_CLASS_CAST ((klass), XED_TYPE_DOCUMENT_OUTPUT_STREAM, XedDocumentOutputStreamClass))
-#define XED_IS_DOCUMENT_OUTPUT_STREAM(obj)		(G_TYPE_CHECK_INSTANCE_TYPE ((obj), XED_TYPE_DOCUMENT_OUTPUT_STREAM))
-#define XED_IS_DOCUMENT_OUTPUT_STREAM_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), XED_TYPE_DOCUMENT_OUTPUT_STREAM))
-#define XED_DOCUMENT_OUTPUT_STREAM_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS ((obj), XED_TYPE_DOCUMENT_OUTPUT_STREAM, XedDocumentOutputStreamClass))
+#define XED_TYPE_DOCUMENT_OUTPUT_STREAM             (xed_document_output_stream_get_type ())
+#define XED_DOCUMENT_OUTPUT_STREAM(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), XED_TYPE_DOCUMENT_OUTPUT_STREAM, XedDocumentOutputStream))
+#define XED_DOCUMENT_OUTPUT_STREAM_CONST(obj)       (G_TYPE_CHECK_INSTANCE_CAST ((obj), XED_TYPE_DOCUMENT_OUTPUT_STREAM, XedDocumentOutputStream const))
+#define XED_DOCUMENT_OUTPUT_STREAM_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), XED_TYPE_DOCUMENT_OUTPUT_STREAM, XedDocumentOutputStreamClass))
+#define XED_IS_DOCUMENT_OUTPUT_STREAM(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), XED_TYPE_DOCUMENT_OUTPUT_STREAM))
+#define XED_IS_DOCUMENT_OUTPUT_STREAM_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), XED_TYPE_DOCUMENT_OUTPUT_STREAM))
+#define XED_DOCUMENT_OUTPUT_STREAM_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), XED_TYPE_DOCUMENT_OUTPUT_STREAM, XedDocumentOutputStreamClass))
 
-typedef struct _XedDocumentOutputStream		XedDocumentOutputStream;
-typedef struct _XedDocumentOutputStreamClass		XedDocumentOutputStreamClass;
-typedef struct _XedDocumentOutputStreamPrivate	XedDocumentOutputStreamPrivate;
+typedef struct _XedDocumentOutputStream         XedDocumentOutputStream;
+typedef struct _XedDocumentOutputStreamPrivate  XedDocumentOutputStreamPrivate;
+typedef struct _XedDocumentOutputStreamClass    XedDocumentOutputStreamClass;
 
 struct _XedDocumentOutputStream
 {
-	GOutputStream parent;
+    GOutputStream parent;
 
-	XedDocumentOutputStreamPrivate *priv;
+    XedDocumentOutputStreamPrivate *priv;
 };
 
 struct _XedDocumentOutputStreamClass
 {
-	GOutputStreamClass parent_class;
+    GOutputStreamClass parent_class;
 };
 
-GType			 xed_document_output_stream_get_type		(void) G_GNUC_CONST;
+GType xed_document_output_stream_get_type (void) G_GNUC_CONST;
 
-GOutputStream		*xed_document_output_stream_new		(XedDocument *doc);
+GOutputStream *xed_document_output_stream_new (XedDocument *doc);
 
 XedDocumentNewlineType xed_document_output_stream_detect_newline_type (XedDocumentOutputStream *stream);
 
