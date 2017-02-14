c27e7811328c7b43eb995fee88aabe2a0328c42e
JosephMcc <mccullarjoseph@gmail.com>
2017-02-13 16:35:38 -0800

    filebrowser: Fix crash on open_terminal action
    https://github.com/GNOME/gedit/commit/0836fd05ac7887d9d437696b4162b536e8134778

--- plugins/filebrowser/xed-file-browser-plugin.c
+++ plugins/filebrowser/xed-file-browser-plugin.c
@@ -487,7 +487,7 @@ add_popup_ui (XedFileBrowserPlugin *plugin)
     gtk_action_group_add_actions (action_group,
                                   extra_single_selection_actions,
                                   G_N_ELEMENTS (extra_single_selection_actions),
-                                  priv->window);
+                                  plugin);
     gtk_ui_manager_insert_action_group (manager, action_group, 0);
     priv->single_selection_action_group = action_group;
 
