a396c5467edb0c91ae33240b7a5d8c5c3d8dd46e
JosephMcc <mccullarjoseph@gmail.com>
2017-09-24 15:12:06 -0700

    preferences-dialog: Complete revamp
    Use the new XApps preferences window as the base for a total redesign of the
    new dialog. While at it port to GtkSourceStyleSchemeChooserWidget.

--- xed/xed-preferences-dialog.h
+++ xed/xed-preferences-dialog.h
@@ -3,7 +3,7 @@
  * xed-preferences-dialog.c
  * This file is part of xed
  *
- * Copyright (C) 2001-2005 Paolo Maggi 
+ * Copyright (C) 2001-2005 Paolo Maggi
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,14 +17,14 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, 
- * Boston, MA 02110-1301, USA. 
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 
 /*
- * Modified by the xed Team, 2003. See the AUTHORS file for a 
- * list of people on the xed Team.  
- * See the ChangeLog files for a list of changes. 
+ * Modified by the xed Team, 2003. See the AUTHORS file for a
+ * list of people on the xed Team.
+ * See the ChangeLog files for a list of changes.
  *
  * $Id$
  */
@@ -32,54 +32,13 @@
 #ifndef __XED_PREFERENCES_DIALOG_H__
 #define __XED_PREFERENCES_DIALOG_H__
 
+#include <libxapp/xapp-preferences-window.h>
+
 #include "xed-window.h"
 
 G_BEGIN_DECLS
 
-/*
- * Type checking and casting macros
- */
-#define XED_TYPE_PREFERENCES_DIALOG              (xed_preferences_dialog_get_type())
-#define XED_PREFERENCES_DIALOG(obj)              (G_TYPE_CHECK_INSTANCE_CAST((obj), XED_TYPE_PREFERENCES_DIALOG, XedPreferencesDialog))
-#define XED_PREFERENCES_DIALOG_CONST(obj)        (G_TYPE_CHECK_INSTANCE_CAST((obj), XED_TYPE_PREFERENCES_DIALOG, XedPreferencesDialog const))
-#define XED_PREFERENCES_DIALOG_CLASS(klass)      (G_TYPE_CHECK_CLASS_CAST((klass), XED_TYPE_PREFERENCES_DIALOG, XedPreferencesDialogClass))
-#define XED_IS_PREFERENCES_DIALOG(obj)           (G_TYPE_CHECK_INSTANCE_TYPE((obj), XED_TYPE_PREFERENCES_DIALOG))
-#define XED_IS_PREFERENCES_DIALOG_CLASS(klass)   (G_TYPE_CHECK_CLASS_TYPE ((klass), XED_TYPE_PREFERENCES_DIALOG))
-#define XED_PREFERENCES_DIALOG_GET_CLASS(obj)    (G_TYPE_INSTANCE_GET_CLASS((obj), XED_TYPE_PREFERENCES_DIALOG, XedPreferencesDialogClass))
-
-
-/* Private structure type */
-typedef struct _XedPreferencesDialogPrivate XedPreferencesDialogPrivate;
-
-/*
- * Main object structure
- */
-typedef struct _XedPreferencesDialog XedPreferencesDialog;
-
-struct _XedPreferencesDialog 
-{
-	GtkDialog dialog;
-	
-	/*< private > */
-	XedPreferencesDialogPrivate *priv;
-};
-
-/*
- * Class definition
- */
-typedef struct _XedPreferencesDialogClass XedPreferencesDialogClass;
-
-struct _XedPreferencesDialogClass 
-{
-	GtkDialogClass parent_class;
-};
-
-/*
- * Public methods
- */
-GType		 xed_preferences_dialog_get_type	(void) G_GNUC_CONST;
-
-void		 xed_show_preferences_dialog		(XedWindow *parent);
+void xed_show_preferences_dialog (XedWindow *parent);
 
 G_END_DECLS
 
